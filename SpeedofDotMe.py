459480016b93f7f9d9321ea768aa1dd9466e786a
Lionel H <lionel_dell24@hotmail.be>
2017-09-14 19:00:05 +0200

    speedtest provider: better time handling
     - Avoid sleep(), this is bad.
     - Improve error handling
    
    Signed-off-by: Lionel H <lionel_dell24@hotmail.be>

--- SpeedofDotMe.py
+++ SpeedofDotMe.py
@@ -7,13 +7,18 @@ class SpeedofDotMe(SpeedtestProvider):
         super().__init__("http://speedof.me")
 
     def run(self):
-       startbtn = self.driver.find_element_by_css_selector('button#btnStart')
-       startbtn.click()
-       print("[+] running speedof.me, please wait")
-       time.sleep(70)
-       print("[+] done, taking snapshot of the website results")
-       self.driver.get_screenshot_as_file('speedofme-results.png') 
-       self.driver.close()
-        
+        try:
+            startbtn = self.driver.find_element_by_css_selector('button#btnStart')
+            startbtn.click()
+            print("[+] running speedof.me, please wait")
+            self.wait_for_clickable('button#btnStart')
+            print("[+] done, taking snapshot of the website results")
+            self.driver.get_screenshot_as_file('speedofme-results.png')
+        except:
+            print("An error occured, cleaning up...")
+            self.cleanup(-1)
+
+        self.cleanup()
+
     def parseResults(self):
         pass
