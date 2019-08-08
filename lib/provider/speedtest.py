5db3d90e3563c3eb45ceec3d9cf429ffdf91fc64
Lionel H <lionel.hubaut@tessares.net>
2019-08-08 17:02:47 +0200

    speedtest: dismiss modal
    Signed-off-by: Lionel H <lionel.hubaut@tessares.net>

--- lib/provider/speedtest.py
+++ lib/provider/speedtest.py
@@ -5,6 +5,7 @@ speedtest.net provider
 # Disable broad-except for now, will refine later.
 # pylint: disable=broad-except
 
+from selenium.common.exceptions import NoSuchElementException
 from provider import Provider
 
 
@@ -20,24 +21,27 @@ class Speedtest(Provider):
         Speedtest will ask you to accept license agreement before launching the test
         This function will click on the "I consent" button to allow test to proceed.
         """
-        self.driver.execute_script(
-            "document.querySelector(\"#_evidon-banner-acceptbutton\").click();")
+        try:
+            if self.driver.find_element_by_css_selector("#_evidon-banner-acceptbutton"):
+                self.driver.execute_script(
+                    "document.querySelector(\"#_evidon-banner-acceptbutton\").click();")
+        except NoSuchElementException:
+            pass #Silently ignore this exception which can occured.
 
-    def run(self):
+    def run(self, filename='speedtest-result.png'):
         try:
             self.accept_eula()
-            self.wait_for_clickable('span.start-text')
-            gobtn = self.driver.find_element_by_css_selector('span.start-text')
-            gobtn.click()
+            self.wait_for_clickable('a.js-start-test')
+            self.driver.find_element_by_css_selector('a.js-start-test').click()
             print("[+] running speedtest.net, please wait")
             # Block until Go btn is available again
-            self.wait_for_clickable('span.start-text')
+            self.wait_for_clickable('a.js-start-test')
             print("[+] done, taking snapshot of the website results")
-            self.driver.get_screenshot_as_file('speedtest-results.png')
+            self.driver.get_screenshot_as_file(filename)
+            # Dismiss speedtest modal.
+            self.driver.find_element_by_css_selector("a.notification-dismiss").click()
         except Exception as exp:
             self.cleanup(-1, exp)
-
-        self.cleanup()
 
     def parse_results(self):
         pass
